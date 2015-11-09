0b2df1664d3e65375a12fe21fc5be2467c847601
Sarah Van Wart <vanwars@airbears2-10-142-143-153.airbears2.1918.berkeley.edu>
2015-11-09 14:27:25 -0800

    Adding data file

---
+++ data.js
@@ -0,0 +1,80 @@
+var data = {
+    "count": 3,
+    "results": [
+        {
+            "url": "http://dev.localground.org/api/0/markers/49/.json",
+            "id": 49,
+            "name": "Banh Mi Sandwich + Vietnamese Noodles: $5.00",
+            "description": "Danish icing cake dessert tiramisu macaroon. Liquorice danish biscuit biscuit jelly-o brownie. Cake jelly-o jelly-o. Cookie bear claw gummies chocolate cake apple pie croissant chupa chups. Caramels cheesecake fruitcake fruitcake muffin donut. Liquorice danish biscuit biscuit jelly-o brownie. Danish icing cake dessert tiramisu macaroon.",
+            "overlay_type": "marker",
+            "tags": "oakland",
+            "owner": "vanwars",
+            "project_id": 24,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.28864669799805,
+                    37.82090404811055
+                ]
+            },
+            "color": "2874F7",
+            "extras": {
+                "name": "Kasey, Chinatown"
+            },
+            "photo_count": 3,
+            "audio_count": 0,
+            "record_count": 0,
+            "map_image_count": 0
+        },
+        {
+            "url": "http://dev.localground.org/api/0/markers/49/.json",
+            "id": 49,
+            "name": "Poor Boy: $4.40",
+            "description": "Danish icing cake dessert tiramisu macaroon. Liquorice danish biscuit biscuit jelly-o brownie. Cake jelly-o jelly-o. Cookie bear claw gummies chocolate cake apple pie croissant chupa chups. Caramels cheesecake fruitcake fruitcake muffin donut. Liquorice danish biscuit biscuit jelly-o brownie. Danish icing cake dessert tiramisu macaroon.",
+            "overlay_type": "marker",
+            "tags": "oakland",
+            "owner": "vanwars",
+            "project_id": 24,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.28864669799805,
+                    37.82090404811055
+                ]
+            },
+            "color": "2874F7",
+            "extras": {
+                "name": "Linda, East Oakland"
+            },
+            "photo_count": 3,
+            "audio_count": 0,
+            "record_count": 0,
+            "map_image_count": 0
+        },
+        {
+            "url": "http://dev.localground.org/api/0/markers/49/.json",
+            "id": 49,
+            "name": "Juice: $5.00",
+            "description": "Danish icing cake dessert tiramisu macaroon. Liquorice danish biscuit biscuit jelly-o brownie. Cake jelly-o jelly-o. Cookie bear claw gummies chocolate cake apple pie croissant chupa chups. Caramels cheesecake fruitcake fruitcake muffin donut. Liquorice danish biscuit biscuit jelly-o brownie. Danish icing cake dessert tiramisu macaroon.",
+            "overlay_type": "marker",
+            "tags": "oakland",
+            "owner": "vanwars",
+            "project_id": 24,
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -122.28864669799805,
+                    37.82090404811055
+                ]
+            },
+            "color": "2874F7",
+            "extras": {
+                "name": "Giovanni, Emeryville"
+            },
+            "photo_count": 3,
+            "audio_count": 0,
+            "record_count": 0,
+            "map_image_count": 0
+        }
+    ]
+};
