9bb3d707752da08b1aacf424275b2e95fba9cf68
Sarah Van Wart <vanwars@Sarahs-MacBook-Pro-3.local>
2015-11-15 16:45:47 -0800

    Change to config file as a result of changes to WDK

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
 var datasets = {
-    all: {
+    fdc_data: {
         api_endpoint: 'http://dev.localground.org/api/0/markers/',
         page_size: 50,
-        filter_text: "WHERE project = 24"
-    },
-    oakland: {
-        api_endpoint: 'http://dev.localground.org/api/0/markers/',
-        page_size: 50,
-        filter_text: "WHERE project = 24 and tags = 'oakland'"
+        server_query: "WHERE project = 24"
     }
 };
 var pages = [
@@ -16,23 +11,26 @@ var pages = [
         accessToken: "pk.eyJ1IjoibGF1cmVuYmVuaWNob3UiLCJhIjoiQ1BlZGczRSJ9.EVMieITn7lHNi6Ato9wFwg",
         styleID: "laurenbenichou.54e91cf8",
         markerSymbol: "restaurant", /* https://www.mapbox.com/maki/ */
-        dataset: "oakland",
+        dataset: "fdc_data",
         clickRoute: "places"
     },
     {
+        type: "list",
         url: "regions/:city",
         collection_template_path: "place-list.html",
         item_template_path: "place-item.html",
         region: '#infoBoxGrid',
-        type: "list",
-        dataset: "oakland",
-        filter_text: "WHERE project = 24 and tags = ':city'" /* Todo: move filters back to list + param substitution */
+        dataset: "fdc_data",
+        client_query: "WHERE tags contains :city"
     },
     {
+        type: "detail",
         url: "places/:id",
         template_path: "place-detail.html",
         region: '#infoBoxGrid',
-        type: "detail",
-        dataset: "all"
+        dataset: "fdc_data",
+        onLoad: function () {
+            pages[0].view.centerMap();
+        }
     }
 ];
