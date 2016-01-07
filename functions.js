75b74bfcb11ccd424a23da8e45909776fe013baf
Sarah <vanwars@gmail.com>
2016-01-06 16:14:25 -0800

    added some instructions / documentation

--- functions.js
+++ functions.js
@@ -1,8 +1,14 @@
 location.hash = ''; //make sure the page initializes on the first page...
 
-var addSearchEventHandler = function() {
+var addSearchEventHandler = function () {
+    /*
+     * Adds button even handler, which navigates to the
+     * region of interest, based on the city
+     * the user wants to search for (from the textbox)
+     */
     $(".search-button").click(function (e) {
-        var text = $(this).prev().val();
+        var $input = $(this).prev(),
+            text = $input.val();
         window.location.hash = "#/regions/" + text;
         $(".search-button").prev().val(text);
         e.preventDefault();
@@ -10,11 +16,17 @@ var addSearchEventHandler = function() {
 };
 
 var showSplash = function () {
+    /*
+     * Shows the splash page; everything else is greyed out.
+     */
     $(".splash").show();
     $("#topSearch, #infoBoxGrid").hide();
 };
 
 var hideSplash = function () {
+    /*
+     * Hides the splash page; users can see the main app
+     */
     $(".splash").hide();
     $("#topSearch, #infoBoxGrid").show();
 };
